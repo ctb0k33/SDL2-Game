82f664ebc291989efbefc5b04788b330658f1ccc
ctb0k33 <96061934+ctb0k33@users.noreply.github.com>
2022-03-14 01:47:03 +0700

    Update MainObject.h
    update 1

--- MainObject.h
+++ MainObject.h
@@ -2,17 +2,23 @@
 #ifndef MAIN_OBJECT_H_
 #define MAIN_OBJECT_H_
 
+#include<vector>
 #include"CommonFunction.h"
 #include"BaseObject.h"
+#include"BulletObject.h"
 
-
+#define GRAVITY_SPEED 0.8
+#define MAX_FALL_SPEED 10
+#define PLAYER_SPEED 8
+#define PLAYER_JUMP_VAL 18
 class MainObject : public BaseObject {
 public:
 	MainObject();
 	~MainObject();
 	enum WalkType {
-		WALK_RIGHT = 0,
-		WALK_LEFT = 1,
+		WALK_NONE = 0,
+		WALK_RIGHT = 1,
+		WALK_LEFT = 2,
 	};
 
 	bool LoadImg(std::string path, SDL_Renderer* screen);
@@ -20,7 +26,29 @@ public:
 	void HandleInputAction(SDL_Event events, SDL_Renderer* screen);
 	void set_clips();
 
+	void DoPlayer(Map& map_data);
+	void CheckToMap(Map& map_data);
+	void SetMapXY(const int map_x, const int map_y) { map_x_ = map_x; map_y_ = map_y; }
+	void CenterEntityOnMap(Map& map_data);
+
+	void UpdateImagePlayer(SDL_Renderer* des);
+
+	SDL_Rect GetRectFrame();
+
+	void set_bullet_list(std::vector<BulletObject*> bullet_list_) {
+		p_bullet_list_ = bullet_list_;
+	}
+	std::vector<BulletObject*> get_bullet_list()const { return p_bullet_list_; }
+	void HandleBullet(SDL_Renderer* des);
+	void RemoveBulelt(const int& idx);
+
+	void IncreaseMoney();
+
+
 private:
+	int money_count;
+
+	std::vector<BulletObject*>p_bullet_list_;
 	float x_val_;
 	float y_val_;
 
@@ -32,10 +60,14 @@ private:
 
 	SDL_Rect frame_clip_[8];
 	Input input_type_;
-	int frame_;
-	int status_;
+	int frame_;// dang o frame nao 
+	int status_;// left or right 
 
+	bool on_ground_;
 
+	int map_x_;
+	int map_y_;
 
+	int come_back_time_;
 };
 #endif
