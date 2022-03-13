c0f0bf3e5ec5315710a8e4aabf4622223139b736
ctb0k33 <96061934+ctb0k33@users.noreply.github.com>
2022-03-14 01:44:37 +0700

    Update CommonFunction.h
    update 1

--- CommonFunction.h
+++ CommonFunction.h
@@ -4,6 +4,7 @@
 #include<Windows.h>
 #include<string>
 #include<SDL.h>
+#include<vector>
 #include<SDL_image.h>
 #include<SDL_mixer.h>
 #include<SDL_ttf.h>
@@ -13,6 +14,8 @@ static SDL_Renderer* g_screen = NULL;
 static SDL_Event g_event;
 
 // Screen
+
+const int FRAME_PER_SECOND = 30; // FPS
 const int SCREEN_WIDTH = 1280;
 const int SCREEN_HEIGHT = 640;
 const int SCREEN_BPP = 32; // PIXEL
@@ -22,10 +25,13 @@ const int COLOR_KEY_G = 175;
 const int COLOR_KEY_B = 180;
 const int RENDER_DRAW_COLOR = 0xff;
 
-
+#define BLANK_TILE 0
 #define TILE_SIZE 64
 #define MAX_MAP_X 400
 #define MAX_MAP_Y 10
+
+
+#define STATE_MONEY 4
 
 typedef struct Input {
 	int left_;
@@ -45,4 +51,10 @@ typedef struct Map {
 	const char* file_name_;
 
 };
+
+
+
+namespace SDLCommonFunc {
+	bool CheckCollision(const SDL_Rect& object1, const SDL_Rect& object2);
+}
 #endif
