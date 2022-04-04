cd0dc1aaac02cebcea4ac5fca7e5ff61352d1fab
ctb0k33 <96061934+ctb0k33@users.noreply.github.com>
2022-04-04 10:26:58 +0700

    Last update

--- main.cpp
+++ main.cpp
@@ -40,6 +40,19 @@ bool InitData() {
 				success = false;
 			}
 		}
+		if (Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096) == -1) {
+			return false;
+		}
+		//Read audio 
+		g_game_sound[0] = Mix_LoadWAV("Debug//sound//gamesound.wav");
+		g_sound_bullet[0] = Mix_LoadWAV("Debug//sound//Gun+Silencer.wav");
+		g_sound_bullet[1] = Mix_LoadWAV("Debug//sound//Explosion+1.wav");
+		g_sound_bullet[2] = Mix_LoadWAV("Debug//sound//Explosion+3.wav");
+		g_sound_exp[0] = Mix_LoadWAV("Debug//sound//Recording (6).wav");
+		g_coin[0] = Mix_LoadWAV("Debug//sound//coin.wav");
+		if (g_sound_bullet[0] == NULL || g_sound_bullet[1] == NULL||g_sound_bullet[2]==NULL||g_game_sound[0]==NULL) {
+			return false;
+		}
 		if (TTF_Init() == -1) {
 			success = false;
 		}
@@ -54,7 +67,7 @@ bool InitData() {
 	return success;
 }
 bool LoadBackground() {
-	bool ret = g_background.LoadImg("Debug//image//loaded.png", g_screen);
+	bool ret = g_background.LoadImg("Debug//image//background4.png", g_screen);
 	if (ret == false) {
 		return false;
 	}
@@ -75,8 +88,8 @@ void close() {
 std::vector<ThreatsObject*> MakeThreadList() {
 	std::vector<ThreatsObject*> list_threats;
 
-	ThreatsObject* dynamic_threats = new ThreatsObject[20];
-	for (int i = 0; i < 20; i++) {
+	ThreatsObject* dynamic_threats = new ThreatsObject[40];
+	for (int i = 1; i < 40; i++) {
 		ThreatsObject* p_threat = (dynamic_threats + i);
 		if (p_threat != NULL) {
 			p_threat->LoadImg("Debug//image//threat_left.png", g_screen);
@@ -89,6 +102,8 @@ std::vector<ThreatsObject*> MakeThreadList() {
 			int pos2 = p_threat->get_x_pos() + 60;
 			p_threat->SetAnimationPos(pos1, pos2);
 			p_threat->set_input_left(1);
+			BulletObject* p_bullet = new BulletObject();
+			p_threat->InitBullet(p_bullet, g_screen);
 			list_threats.push_back(p_threat);
 		}
 	}
@@ -104,7 +119,6 @@ std::vector<ThreatsObject*> MakeThreadList() {
 			p_threat->set_y_pos(250);
 			p_threat->set_type_move(ThreatsObject::STATIC_THREAT);
 			p_threat->set_input_left(0);
-
 			BulletObject* p_bullet = new BulletObject();
 			p_threat->InitBullet(p_bullet, g_screen);
 			list_threats.push_back(p_threat);
@@ -114,7 +128,7 @@ std::vector<ThreatsObject*> MakeThreadList() {
 	return list_threats;
 }
 int main(int argc, char* agrv[]) {
-
+	bool game_win = false;
 	ImpTimer fps_timer;
 
 	if (InitData() == false)
@@ -124,7 +138,7 @@ int main(int argc, char* agrv[]) {
 
 
 	GameMap game_map;
-	game_map.LoadMap("map/map good/map01.dat");
+	game_map.LoadMap("map_type4//map.dat");
 	game_map.LoadTiles(g_screen);
 
 
@@ -137,7 +151,7 @@ int main(int argc, char* agrv[]) {
 
 	PlayerMoney player_money;
 	player_money.Init(g_screen);
-	player_money.SetPos(SCREEN_WIDTH * 0.5 - 300, 8);
+	player_money.SetPos(SCREEN_WIDTH * 0.5-285, 7);
 
 	std::vector<ThreatsObject*> threats_list = MakeThreadList();
 
@@ -149,7 +163,7 @@ int main(int argc, char* agrv[]) {
 	bossObject.set_xpos(xPosBoss);
 	bossObject.set_ypos(10);
 
-
+	BossHp boss_hp;
 
 	//explosion
 	ExplosionObject exp_threat,exp_main;
@@ -178,20 +192,29 @@ int main(int argc, char* agrv[]) {
 	TextObject money_game;
 	money_game.SetColor(TextObject::WHILE_TEXT);
 
+	TextObject boss_name;
+	boss_name.SetColor(TextObject::BLACK_TEXT);
 	int bulletcheck = 0; // check for boss
 	int hit_boss = 0; // boss life
-
+	int condition_appear = 0; // boss appear
 
 	bool is_quit = false;
+	Mix_PlayChannel(-1, g_game_sound[0], 0);
 	while (!is_quit)
 	{
+		if (game_win == true) {
+			if (MessageBox(NULL, L"YOU WIN", L"Info", MB_OK | MB_ICONSTOP) == IDOK) {
+				is_quit = true;
+				break;
+			}
+		}
 		fps_timer.start();
 		while (SDL_PollEvent(&g_event) != 0)
 		{
 			if (g_event.type == SDL_QUIT) {
 				is_quit = true;
 			}
-			p_player.HandleInputAction(g_event, g_screen);
+			p_player.HandleInputAction(g_event, g_screen,g_sound_bullet,g_sound_exp);
 		}
 		SDL_SetRenderDrawColor(g_screen, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR);
 		SDL_RenderClear(g_screen);
@@ -203,7 +226,7 @@ int main(int argc, char* agrv[]) {
 
 		p_player.HandleBullet(g_screen); // +x_val for bullet
 		p_player.SetMapXY(map_data.start_x_, map_data.start_y_);
-		p_player.DoPlayer(map_data);
+		p_player.DoPlayer(map_data, g_coin);
 		p_player.Show(g_screen);
 		game_map.SetMap(map_data);
 		game_map.DrawMap(g_screen);
@@ -245,7 +268,12 @@ int main(int argc, char* agrv[]) {
 				p_threat->SetMapXY(map_data.start_x_, map_data.start_y_);
 				p_threat->ImpMoveType(g_screen);
 				p_threat->DoPlayer(map_data);
-				p_threat->MakeBullet(g_screen, SCREEN_WIDTH, SCREEN_HEIGHT);
+				if (p_threat->get_direct() == 0) {
+					p_threat->MakeBullet(g_screen, SCREEN_WIDTH, SCREEN_HEIGHT);
+				}
+				/*else*/ if (p_threat->get_direct() == 1) {
+					p_threat->MakeBullet2(g_screen, SCREEN_WIDTH, SCREEN_HEIGHT);
+				}
 				p_threat->Show(g_screen);
 				SDL_Rect rect_player = p_player.GetRectFrame();
 				bool bCol1 = false;
@@ -257,7 +285,8 @@ int main(int argc, char* agrv[]) {
 						if (bCol1) {
 							exp_main.start_explosion(p_player.GetRect(), g_screen, exp_main);
 							SDL_RenderPresent(g_screen);
-							p_threat->RemoveBulelt(jj);
+							/*p_threat->RemoveBulelt(jj);*/
+							Mix_PlayChannel(-1, g_sound_bullet[2], 0);
 							break;
 						}
 					}
@@ -270,6 +299,7 @@ int main(int argc, char* agrv[]) {
 					/*	exp_main*/
 					life--;
 					if (life >= 0) {
+						Mix_PlayChannel(-1, g_sound_bullet[2], 0);
 						p_player.SetRect(0, 0);
 						p_player.set_comeback_time(60);
 						SDL_Delay(1000);
@@ -279,6 +309,7 @@ int main(int argc, char* agrv[]) {
 					}
 					else {
 						if (MessageBox(NULL, L"GAME OVER", L"Info", MB_OK | MB_ICONSTOP) == IDOK) {
+							Mix_PlayChannel(-1, g_sound_bullet[2], 0);
 							p_threat->Free();
 							close();
 							SDL_Quit();
@@ -307,7 +338,7 @@ int main(int argc, char* agrv[]) {
 						bool bCol = SDLCommonFunc::CheckCollision(bRect, tRect);
 						if (bCol) {
 							/*	explosion*/
-
+							Mix_PlayChannel(-1, g_sound_bullet[1], 0);
 							mark_value++;
 							exp_threat.start_explosion(bRect, g_screen, exp_threat);
 							p_player.RemoveBulelt(r);
@@ -360,8 +391,22 @@ int main(int argc, char* agrv[]) {
 		money_game.LoadFromRenderText(font_time, g_screen);
 		money_game.RenderText(g_screen, SCREEN_WIDTH * 0.5 - 250, 15);
 		/* show boss */
+		std::string dragon_name = "Stormterror Dvalin";
+		boss_name.SetText(dragon_name);
 		int val = MAX_MAP_X * TILE_SIZE - (map_data.start_x_ + p_player.GetRect().x);
 		if (val <= SCREEN_WIDTH) {
+			condition_appear = 1;
+		}
+		if (condition_appear==1) {
+		/*	map_data.start_x_ = bossObject.get_x_pos() - SCREEN_WIDTH;
+			if (p_player.get_x_pos_() < map_data.start_x_) {
+				p_player.set_x_val_();
+			}*/
+			boss_name.LoadFromRenderText(font_time, g_screen);
+			boss_name.RenderText(g_screen, SCREEN_WIDTH * 0.5 - 100, 42);
+			boss_hp.Init(g_screen);
+			boss_hp.SetRect(SCREEN_WIDTH/2-400, 60);
+			boss_hp.Show(g_screen);
 			bossObject.SetMapXY(map_data.start_x_, map_data.start_y_);
 			bossObject.DoPlayer(map_data);
 			if (bulletcheck == 0) {
@@ -373,14 +418,21 @@ int main(int argc, char* agrv[]) {
 				BulletObject* p_bullet = bullet_arr.at(i);
 				bool bcol3 = SDLCommonFunc::CheckCollision(bossObject.GetRect(), p_bullet->GetRect());
 				if (bcol3 == true) {
+					boss_hp.Decrease();
+					Mix_PlayChannel(-1, g_sound_bullet[1], 0);
 					exp_threat.start_explosion(p_bullet->GetRect(), g_screen, exp_threat);
 					hit_boss++;
-					if (hit_boss >= 5) {
+					p_player.RemoveBulelt(i);
+					if (hit_boss == 5) {
 						exp_threat.start_explosion(p_bullet->GetRect(), g_screen, exp_threat);
-						p_player.RemoveBulelt(i);
 						bossObject.RemoveBullet();
 						bossObject.Free();
+						boss_hp.Init(g_screen);
+						boss_hp.Show(g_screen);
 						bulletcheck = 1;
+						game_win = true;
+						SDL_Delay(1000);
+						
 					}
 				}
 			}
@@ -389,6 +441,7 @@ int main(int argc, char* agrv[]) {
 				BulletObject* b_bullet = bossObject.get_bullet_list().at(i);
 				bool bcol5 = SDLCommonFunc::CheckCollision(p_player.GetRectFrame(), b_bullet->GetRect());
 				if (bcol5 || bcol4) {
+					Mix_PlayChannel(-1, g_sound_bullet[2], 0);
 					exp_main.start_explosion(p_player.GetRect(), g_screen, exp_main);
 					SDL_RenderPresent(g_screen);
 					/*	exp_main*/
@@ -433,5 +486,13 @@ int main(int argc, char* agrv[]) {
 	close();
 	return 0;
 }
+
+
+
+
+
+
+
+
 
 
